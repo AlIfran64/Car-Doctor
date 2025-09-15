db8dbf4ec2ed59d66327d3972a12db57134100ee
Al Ifran <ifran15-2915@diu.edu.bd>
2025-09-15 17:08:22 +0600

    navbar done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,43 +1,170 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RxCross2 } from "react-icons/rx";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className='w-11/12 mx-auto py-5 flex justify-between items-center'>
-
-      {/* Left */}
-      <div>
-        <Image src={"/assets/logo.svg"} width={80} height={80} alt='logo' />
-      </div>
-
-      {/* Middle */}
-      <div className='space-x-6 text-base font-semibold'>
-        <Link className='hover:text-[#FF3811]' href={'/'}>Home</Link>
-        <Link className='hover:text-[#FF3811]' href={'/about'}>About</Link>
-        <Link className='hover:text-[#FF3811]' href={'/services'}>Services</Link>
-        <Link className='hover:text-[#FF3811]' href={'/blog'}>Blog</Link>
-        <Link className='hover:text-[#FF3811]' href={'/contact'}>Contact</Link>
-      </div>
-
-      {/* Right */}
-      <div className='flex items-center justify-end space-x-3'>
-
-        {/* Shopping cart */}
+    <div>
+      {/* Desktop */}
+      <div className="w-11/12 mx-auto py-5 hidden lg:flex justify-between items-center">
+        {/* Left */}
         <div>
-          <svg className='cursor-pointer' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-            <path d="M15.75 10.5V6C15.75 5.00544 15.3549 4.05161 14.6516 3.34835C13.9484 2.64509 12.9945 2.25 12 2.25C11.0054 2.25 10.0516 2.64509 9.34833 3.34835C8.64507 4.05161 8.24998 5.00544 8.24998 6V10.5M19.606 8.507L20.869 20.507C20.939 21.172 20.419 21.75 19.75 21.75H4.24998C4.09219 21.7502 3.93613 21.7171 3.79193 21.6531C3.64774 21.589 3.51863 21.4953 3.41301 21.3781C3.30738 21.2608 3.2276 21.1227 3.17884 20.9726C3.13008 20.8226 3.11343 20.6639 3.12999 20.507L4.39399 8.507C4.42314 8.23056 4.55361 7.9747 4.76024 7.78876C4.96686 7.60281 5.23501 7.49995 5.51298 7.5H18.487C19.063 7.5 19.546 7.935 19.606 8.507ZM8.62498 10.5C8.62498 10.5995 8.58548 10.6948 8.51515 10.7652C8.44482 10.8355 8.34944 10.875 8.24998 10.875C8.15053 10.875 8.05515 10.8355 7.98482 10.7652C7.91449 10.6948 7.87498 10.5995 7.87498 10.5C7.87498 10.4005 7.91449 10.3052 7.98482 10.2348C8.05515 10.1645 8.15053 10.125 8.24998 10.125C8.34944 10.125 8.44482 10.1645 8.51515 10.2348C8.58548 10.3052 8.62498 10.4005 8.62498 10.5V10.5ZM16.125 10.5C16.125 10.5995 16.0855 10.6948 16.0152 10.7652C15.9448 10.8355 15.8494 10.875 15.75 10.875C15.6505 10.875 15.5551 10.8355 15.4848 10.7652C15.4145 10.6948 15.375 10.5995 15.375 10.5C15.375 10.4005 15.4145 10.3052 15.4848 10.2348C15.5551 10.1645 15.6505 10.125 15.75 10.125C15.8494 10.125 15.9448 10.1645 16.0152 10.2348C16.0855 10.3052 16.125 10.4005 16.125 10.5V10.5Z" stroke="#444444" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-          </svg>
+          <Image src={"/assets/logo.svg"} width={80} height={80} alt="logo" />
         </div>
 
-        {/* Search */}
-        <div>
-          <svg className='cursor-pointer' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-            <path d="M21 21L15.803 15.803M15.803 15.803C17.2096 14.3964 17.9998 12.4887 17.9998 10.4995C17.9998 8.51029 17.2096 6.60256 15.803 5.19599C14.3964 3.78941 12.4887 2.99921 10.4995 2.99921C8.51029 2.99921 6.60256 3.78941 5.19599 5.19599C3.78941 6.60256 2.99921 8.51029 2.99921 10.4995C2.99921 12.4887 3.78941 14.3964 5.19599 15.803C6.60256 17.2096 8.51029 17.9998 10.4995 17.9998C12.4887 17.9998 14.3964 17.2096 15.803 15.803V15.803Z" stroke="#444444" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-          </svg>
+        {/* Middle */}
+        <div className="space-x-10 text-base font-semibold">
+          <Link className="hover:text-[#FF3811]" href={"/"}>Home</Link>
+          <Link className="hover:text-[#FF3811]" href={"/about"}>About</Link>
+          <Link className="hover:text-[#FF3811]" href={"/services"}>Services</Link>
+          <Link className="hover:text-[#FF3811]" href={"/blog"}>Blog</Link>
+          <Link className="hover:text-[#FF3811]" href={"/contact"}>Contact</Link>
         </div>
-        <button className='text-[#FF3811] font-semibold text-lg px-4 py-2 border-2 border-[#FF3811] rounded-lg hover:text-white hover:bg-[#FF3811] cursor-pointer'>Appointment</button>
+
+        {/* Right */}
+        <div className="flex items-center justify-end space-x-3">
+          {/* Shopping cart */}
+          <div>
+            <svg
+              className="cursor-pointer hover:text-[#FF3811]"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <path
+                d="M15.75 10.5V6C15.75 5.00544 15.3549 4.05161 14.6516 3.34835C13.9484 2.64509 12.9945 2.25 12 2.25C11.0054 2.25 10.0516 2.64509 9.34833 3.34835C8.64507 4.05161 8.24998 5.00544 8.24998 6V10.5M19.606 8.507L20.869 20.507C20.939 21.172 20.419 21.75 19.75 21.75H4.24998C4.09219 21.7502 3.93613 21.7171 3.79193 21.6531C3.64774 21.589 3.51863 21.4953 3.41301 21.3781C3.30738 21.2608 3.2276 21.1227 3.17884 20.9726C3.13008 20.8226 3.11343 20.6639 3.12999 20.507L4.39399 8.507C4.42314 8.23056 4.55361 7.9747 4.76024 7.78876C4.96686 7.60281 5.23501 7.49995 5.51298 7.5H18.487C19.063 7.5 19.546 7.935 19.606 8.507Z"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+
+          {/* Search */}
+          <div>
+            <svg
+              className="cursor-pointer hover:text-[#FF3811]"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <path
+                d="M21 21L15.803 15.803M15.803 15.803C17.2096 14.3964 17.9998 12.4887 17.9998 10.4995C17.9998 8.51029 17.2096 6.60256 15.803 5.19599C14.3964 3.78941 12.4887 2.99921 10.4995 2.99921C8.51029 2.99921 6.60256 3.78941 5.19599 5.19599C3.78941 6.60256 2.99921 8.51029 2.99921 10.4995C2.99921 12.4887 3.78941 14.3964 5.19599 15.803C6.60256 17.2096 8.51029 17.9998 10.4995 17.9998C12.4887 17.9998 14.3964 17.2096 15.803 15.803Z"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+
+          <button className="text-[#FF3811] font-semibold text-lg px-4 py-2 border-2 border-[#FF3811] rounded-lg hover:text-white hover:bg-[#FF3811] cursor-pointer">
+            Appointment
+          </button>
+        </div>
+      </div>
+
+      {/* Mobile */}
+      <div className="w-11/12 mx-auto py-5 flex lg:hidden justify-between items-center">
+        {/* Left */}
+        <div>
+          <Image src={"/assets/logo.svg"} width={50} height={50} alt="logo" />
+        </div>
+
+        {/* Right */}
+        <div className="flex items-center space-x-3">
+          <div className="flex items-center justify-end space-x-3">
+            {/* Shopping cart */}
+            <div>
+              <svg
+                className="cursor-pointer hover:text-[#FF3811]"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+              >
+                <path
+                  d="M15.75 10.5V6C15.75 5.00544 15.3549 4.05161 14.6516 3.34835C13.9484 2.64509 12.9945 2.25 12 2.25C11.0054 2.25 10.0516 2.64509 9.34833 3.34835C8.64507 4.05161 8.24998 5.00544 8.24998 6V10.5M19.606 8.507L20.869 20.507C20.939 21.172 20.419 21.75 19.75 21.75H4.24998C4.09219 21.7502 3.93613 21.7171 3.79193 21.6531C3.64774 21.589 3.51863 21.4953 3.41301 21.3781C3.30738 21.2608 3.2276 21.1227 3.17884 20.9726C3.13008 20.8226 3.11343 20.6639 3.12999 20.507L4.39399 8.507C4.42314 8.23056 4.55361 7.9747 4.76024 7.78876C4.96686 7.60281 5.23501 7.49995 5.51298 7.5H18.487C19.063 7.5 19.546 7.935 19.606 8.507Z"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
+
+            {/* Search */}
+            <div>
+              <svg
+                className="cursor-pointer hover:text-[#FF3811]"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+              >
+                <path
+                  d="M21 21L15.803 15.803M15.803 15.803C17.2096 14.3964 17.9998 12.4887 17.9998 10.4995C17.9998 8.51029 17.2096 6.60256 15.803 5.19599C14.3964 3.78941 12.4887 2.99921 10.4995 2.99921C8.51029 2.99921 6.60256 3.78941 5.19599 5.19599C3.78941 6.60256 2.99921 8.51029 2.99921 10.4995C2.99921 12.4887 3.78941 14.3964 5.19599 15.803C6.60256 17.2096 8.51029 17.9998 10.4995 17.9998C12.4887 17.9998 14.3964 17.2096 15.803 15.803Z"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
+          </div>
+
+          {/* Hamburger and cancel function */}
+          {menuOpen ? (
+            <div>
+              <div className="absolute top-0 right-0 h-screen w-50 bg-white shadow-lg p-5 space-y-3 text-base font-semibold">
+
+                {/* Cancel btn */}
+                <div className="flex justify-end">
+                  <RxCross2
+                    onClick={() => setMenuOpen(false)}
+                    size={28}
+                    className="cursor-pointer"
+                  />
+                </div>
+
+                {/* Navigation */}
+                <div className="flex flex-col text-center space-y-3">
+                  <Link href={"/"} onClick={() => setMenuOpen(false)}>Home</Link>
+                  <Link href={"/about"} onClick={() => setMenuOpen(false)}>About</Link>
+                  <Link href={"/services"} onClick={() => setMenuOpen(false)}>Services</Link>
+                  <Link href={"/blog"} onClick={() => setMenuOpen(false)}>Blog</Link>
+                  <Link href={"/contact"} onClick={() => setMenuOpen(false)}>Contact</Link>
+                </div>
+
+                {/* other btn */}
+                <div className="flex justify-center">
+                  <button className="text-[#FF3811] font-semibold text-base px-4 py-1.5 border-2 border-[#FF3811] rounded-lg hover:text-white hover:bg-[#FF3811] cursor-pointer">
+                    Appointment
+                  </button>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <GiHamburgerMenu
+              onClick={() => setMenuOpen(true)}
+              size={25}
+              className="cursor-pointer"
+            />
+          )}
+        </div>
       </div>
     </div>
-  )
+  );
 }
